2e4d4b77e35263b702493a1d88069abe4cb8326f
CI364475 <ci364475@g.risd.org>
2021-09-09 19:26:11 -0500

    Commit one

---
+++ DupRunner.java
@@ -0,0 +1,5 @@
+public class DupRunner {
+    public static void main(String[] args) {
+        // Add tests
+    }
+}
---
+++ UniquesDupes.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+
+public class UniquesDupes {
+    public static Set<String> getUniques(String input) {
+        // Create a Set of Strings called `uniques` and instantiate it with a TreeSet
+        // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
+        // Add items from `list` to `uniques`
+
+        // Return `uniques`
+        return null;
+    }
+
+    public static Set<String> getDupes(String input) {
+        // Create a Set of Strings called `uniques` and instantiate it with a TreeSet
+        // Create a Set of Strings called `dupes` and instantiate it with a TreeSet
+        // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
+        // Add items from `list` to `uniques` and determine which items should go in `dupes`
+
+        // Return `dupes`
+        return null;
+    }
+}
