205856f40d4e03a46ed8a8b51155a7aeb20ea582
CI364475 <ci364475@g.risd.org>
2021-09-13 21:19:43 -0500

    Done

--- DupRunner.java
+++ DupRunner.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class DupRunner {
     public static void main(String[] args) {
         // Add tests
+        Scanner input = new Scanner(in);
+        String words = input.nextLine();
+
+        out.println("Original List: " + words);
+        out.println("Uniques: " + UniquesDupes.getUniques(words));
+        out.println("Dupes: " + UniquesDupes.getDupes(words));
     }
 }
--- UniquesDupes.java
+++ UniquesDupes.java
@@ -1,13 +1,15 @@
-import java.util.Set;
+import java.util.*;
 
 public class UniquesDupes {
     public static Set<String> getUniques(String input) {
         // Create a Set of Strings called `uniques` and instantiate it with a TreeSet
         // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
         // Add items from `list` to `uniques`
+        String[] words = input.split(" ");
+        List<String> list = new ArrayList<>(Arrays.asList(words));
 
         // Return `uniques`
-        return null;
+        return new TreeSet<>(list);
     }
 
     public static Set<String> getDupes(String input) {
@@ -15,8 +17,18 @@ public class UniquesDupes {
         // Create a Set of Strings called `dupes` and instantiate it with a TreeSet
         // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
         // Add items from `list` to `uniques` and determine which items should go in `dupes`
+        TreeSet<String> uniques = new TreeSet<>();
+        TreeSet<String> dupes = new TreeSet<>();
+        String[] words = input.split(" ");
+        List<String> list = new ArrayList<>(Arrays.asList(words));
+
+        for (String word : list) {
+            if (!uniques.add(word)) {
+                dupes.add(word);
+            }
+        }
 
         // Return `dupes`
-        return null;
+        return dupes;
     }
 }
