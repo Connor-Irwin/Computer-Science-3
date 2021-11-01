e29d3bd9a09413be3d120c76f97fb75f2625b2ac
CI364475 <ci364475@g.risd.org>
2021-11-01 12:36:17 -0500

    Commit

---
+++ MyStack.java
@@ -0,0 +1,68 @@
+/**
+ * This class implements a stack of Integer objects.
+ */
+
+import java.util.EmptyStackException;
+
+public class MyStack {
+    private Integer[] stack;
+    private int size, min, max;
+
+    public MyStack() {
+        this(5);
+    }
+
+    public MyStack(int initCap) {
+        stack = new Integer[initCap];
+        size = 0;
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Integer peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return stack[size - 1];
+    }
+
+    public Integer pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        return stack[--size];
+    }
+
+    public void push(Integer item) {
+        if (size == stack.length) doubleCapacity();
+        stack[size++] = item;
+
+        if (item < min) min = item;
+        if (item > max) max = item;
+    }
+
+    private void doubleCapacity() {
+        Integer[] doubleStack = new Integer[stack.length * 2];
+        System.arraycopy(stack, 0, doubleStack, 0, stack.length);
+        stack = doubleStack;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (Integer i : stack) {
+            if (i != null) {
+                output.insert(0, i + "\n");
+            }
+        }
+        return output.toString();
+    }
+}
