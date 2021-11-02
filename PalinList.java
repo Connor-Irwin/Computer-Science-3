9a5b762243ee67bb39c37e158909bbb1e5ac502a
CI364475 <ci364475@g.risd.org>
2021-11-02 12:42:22 -0500

    Commit

---
+++ PalinList.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class PalinList {
+    private Queue<String> queue;
+    private Stack<String> stack;
+
+    public PalinList() {
+        setList("");
+    }
+
+    public PalinList(String str) {
+        setList(str);
+    }
+
+    public void setList(String str) {
+        queue = new LinkedList<>();
+        stack = new Stack<>();
+
+        for (String word : str.split(" ")) {
+            queue.add(word);
+            stack.push(word);
+        }
+    }
+
+    public boolean isPalin() {
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.poll())) return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "" + stack.toString();
+    }
+}
